da9973dfdd3b2461b18c5feb15cdf08b2ca3f6c1
Pulsatill <arch-angel2002@yandex.ru>
2023-02-07 21:25:48 +0900

    add new function for upper strings

--- features/str_func.py
+++ features/str_func.py
@@ -1,4 +1,9 @@
 
+def upper_str(user_str):
+    """The func returns the received string with uppercase letters"""
+    return upper(lower(user_str))
+
+
 def title_str(user_str):
-    """The function returns the received string with uppercase letters"""
+    """The function returns the received string with uppercase first letters in words"""
     return title(lower(user_str))
