1ce7d03c05ff4fa9ec20027b853cb21c7e9099c6
Pulsatill <arch-angel2002@yandex.ru>
2023-02-11 01:28:14 +0900

    add new function for lower strings

--- features/str_func.py
+++ features/str_func.py
@@ -7,3 +7,6 @@ def upper_str(user_str):
 def title_str(user_str):
     """The function returns the received string with uppercase first letters in words"""
     return title(lower(user_str))
+
+def lower_str(user_str):
+    return lower(user_str)
